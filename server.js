3b25683f255a0035b6a2062787becbc01c63ee6f
laecioandrade <laecio_a.rodrigues@outlook.com>
2020-02-25 18:49:46 -0300

    Desenvolvendo backend.

--- server.js
+++ server.js
@@ -5,17 +5,57 @@ const server = express()
 server.use(express.static('public'))
 
 
+//habilitar body do formulario
+server.use(express.urlencoded({extended: true}))
+
 //configurando a template engine
 const nunjucks = require("nunjucks")
 nunjucks.configure("./", {
-    express: server
+    express: server,
+    noCache: true,
 })
+
+
+//lista de doadores: vetor ou array
+const donors = [
+    {
+        name: "Diego Fernandes",
+        blood: "AB+"
+    },
+    {
+        name: "Cleiton Souza",
+        blood: "B+"
+    },
+    {
+        name: "Robson Marques",
+        blood: "O"
+    },
+    {
+        name: "Mayk Brito",
+        blood: "A-"
+    }
+]
+
 
 //configurar a apresentação da página
 server.get("/", function(req, res){
-    return res.render("index.html")
+    return res.render("index.html", {donors})
 })
 
+server.post("/", function(req, res){ 
+    //pegar dados do formulario
+    const name = req.body.name
+    const email = req.body.email
+    const blood = req.body.blood
+
+    //colocar valores dentro do array
+    donors.push({
+        name: name,
+        blood: blood,
+    })
+
+    return res.redirect("/")
+})
 
 
 //ligar servidor e permitir acesso na porta 3000
