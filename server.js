690340574c51c7dfd4a490be030d7c6aa9667e0e
laecioandrade <laecio_a.rodrigues@outlook.com>
2020-02-26 23:12:19 -0300

    Aplicação web concluida

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ server.use(express.static('public'))
 //habilitar body do formulario
 server.use(express.urlencoded({extended: true}))
 
+//Configurar conexão com o banco de dados
+const Pool = require('pg').Pool
+const db = new Pool({
+    user: 'postgres',
+    password: 'tente01',
+    host: 'localhost',
+    port: 5432,
+    database: 'doe'
+})
+
 //configurando a template engine
 const nunjucks = require("nunjucks")
 nunjucks.configure("./", {
@@ -16,30 +26,14 @@ nunjucks.configure("./", {
 })
 
 
-//lista de doadores: vetor ou array
-const donors = [
-    {
-        name: "Diego Fernandes",
-        blood: "AB+"
-    },
-    {
-        name: "Cleiton Souza",
-        blood: "B+"
-    },
-    {
-        name: "Robson Marques",
-        blood: "O"
-    },
-    {
-        name: "Mayk Brito",
-        blood: "A-"
-    }
-]
-
 
 //configurar a apresentação da página
 server.get("/", function(req, res){
-    return res.render("index.html", {donors})
+    db.query("SELECT * FROM donors", function(err, result){
+        if(err) return res.send("Erro no banco de dados.")
+        const donors = result.rows
+        return res.render("index.html", {donors})
+    })
 })
 
 server.post("/", function(req, res){ 
@@ -48,13 +42,23 @@ server.post("/", function(req, res){
     const email = req.body.email
     const blood = req.body.blood
 
-    //colocar valores dentro do array
-    donors.push({
-        name: name,
-        blood: blood,
-    })
+    if (name == "" || email == "" || blood == ""){
+        return res.send("Todos os campos são obrigatórios.")
+    }
+    
+    //coloco valores dentro do banco de dados
+    const query = `
+    INSERT INTO "donors" ("name", "email", "blood") 
+    VALUES ($1, $2, $3)`
+    
+    const values = [name, email, blood]
 
-    return res.redirect("/")
+    db.query(query, values, function(err){
+        //fluxo de erro
+        if(err) return res.send("erro no banco de dados.")
+        //fluxo ideal
+        return res.redirect("/")
+    })
 })
 
 
